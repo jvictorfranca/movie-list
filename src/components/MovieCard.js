2bd845ca258ab7df775be250fe44dcc93fe8e192
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-08-04 22:12:31 -0300

    arruma as imagens

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,15 +3,20 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Rating from './Rating';
 import '../App.css';
-import Aplessed_Alpa from './images/Appleseed_Alpha.jpg'
+import KingSglaive from './images/Kingsglaive_Final_Fantasy_XV.jpg'
 import Final_Fantasy_Spirits from './images/Final_Fantasy_Spirits_Within.jpg'
-
+import GhostInTheShell from './images/Ghost_In_The_Shell_2_0.jpg'
+import Applessed_Alpha from './images/Appleseed_Alpha.jpg'
+import Resident from './images/Resident_Evil_Vendetta.jpg'
 class MovieCard extends React.Component {
   render() {
     const { props } = this;
     const { title, subtitle, storyline, imagePath, rating, id } = props.movie;
     console.log(imagePath)
     console.log(Final_Fantasy_Spirits)
+    console.log(GhostInTheShell)
+    console.log(KingSglaive)
+    console.log(Resident)
     return (
       <div className="movie-card movie-card-body" data-testid="movie-card">
         <h4 className="movie-card-title" data-testid="movie-card-title">{title}</h4>
