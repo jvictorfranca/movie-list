01569b1a10612bb4e724ff329a279acab62f3402
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-08-06 14:09:39 -0300

    resolve o bug das imagens nao aparecerem

--- src/services/movieData.js
+++ src/services/movieData.js
@@ -1,3 +1,9 @@
+import kingsglaive from './images/Kingsglaive_Final_Fantasy_XV.jpg'
+import finalfantasy from './images/Final_Fantasy_Spirits_Within.jpg'
+import ghostintheshell from './images/Ghost_In_The_Shell_2_0.jpg'
+import aplessedalpha from './images/Appleseed_Alpha.jpg'
+import residentevil from './images/Resident_Evil_Vendetta.jpg'
+
 
 const movies = [
   {
@@ -6,7 +12,7 @@ const movies = [
     subtitle: 'Final Fantasy XV',
     storyline: "King Regis, who oversees the land of Lucis, commands his army of soldiers to protect the kingdom from the Niflheim empire's plans to steal the sacred crystal.",
     rating: 4.5,
-    imagePath:'/movie-list/static/media/Kingsglaive_Final_Fantasy_XV.ec78b774.jpg',
+    imagePath:kingsglaive,
     bookmarked: true,
     genre: 'action',
   },
@@ -16,7 +22,7 @@ const movies = [
     subtitle: 'Spirits Within',
     storyline: 'A scientist makes a last stand on Earth with the help of a ragtag team of soldiers against an invasion of alien phantoms.',
     rating: 4.5,
-    imagePath: '/movie-list/static/media/Final_Fantasy_Spirits_Within.b8097429.jpg',
+    imagePath: finalfantasy,
     bookmarked: false,
     genre: 'fantasy',
   },
@@ -26,7 +32,7 @@ const movies = [
     subtitle: 'Ghost In The Shell',
     storyline: 'A hacker known as the Puppet Master is hunted by a female cyborg cop and her partner. This film is a revised version of Ghost in the Shell (1995).',
     rating: 5,
-    imagePath: '/movie-list/static/media/Ghost_In_The_Shell_2_0.2cabe5f9.jpg',
+    imagePath: ghostintheshell,
     bookmarked: false,
     genre: 'comedy',
   },
@@ -36,7 +42,7 @@ const movies = [
     subtitle: 'Appleseed Alpha',
     storyline: "A young female soldier Deunan and her cyborg partner Briareos survive through the post World War 3 apocalyptic New York in search of human's future hope, the legendary city of Olympus.",
     rating: 3.8,
-    imagePath: '/movie-list/static/media/Appleseed_Alpha.30ae10ef.jpg',
+    imagePath: aplessedalpha,
     bookmarked: true,
     genre: 'action',
   },
@@ -46,7 +52,7 @@ const movies = [
     subtitle: 'Vendetta',
     storyline: 'Chris Redfield enlists the help of Leon S. Kennedy and Rebecca Chambers to stop a death merchant, with a vengeance, from spreading a deadly virus in New York.',
     rating: 4.2,
-    imagePath: '/movie-list/static/media/Resident_Evil_Vendetta.ffeb1d1b.jpg',
+    imagePath: residentevil,
     bookmarked: true,
     genre: 'fantasy',
   },
